7a409e46054363d1ecba8dea7328868c22bb74c8
thamizhsoftsuave <thamizh@softsuave.com>
2022-10-11 22:25:47 +0530

    environmentchanges

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  // BASE_URL: 'http://localhost:3200/',
+  BASE_URL: 'http://3.110.221.128/',
+  API_ENDPOINT: 'api/',
+  mapKey: 'AIzaSyDsmRCwyjiDzB89HJXZbqT3pHvxXt-rE2M',
 };
